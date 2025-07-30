c0670d85b6bb09abeddeba58c064255c5f1c3ed4
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2025-07-30 13:30:15 -0400

    Swap elijah to eli

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
                 <div className={styles["home-txt-container"]}>
                     <div>
                         <p className={styles["home-txt"]}>Hi, my name is</p>
-                        <h2 className={styles["home-name"]}>Elijah Zwiebel</h2>
+                        <h2 className={styles["home-name"]}>Eli Zwiebel</h2>
                         <h1 className={styles["home-title"]}>Software Engineer</h1>
                     </div>
                 </div>
